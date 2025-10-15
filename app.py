5a8a24c8a6750400e8fa995f9c88cdab297e5b8f
Settem Tanvi <144032915+tanv000@users.noreply.github.com>
2025-10-15 16:09:38 +0530

    trigger pipeline

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import datetime
 
 def main():
-    """Prints a simple message and the current time  ."""
+    """Prints a simple message and the current time."""
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"Hello from the basic Python application!")
     print(f"The current UTC time is: {current_time}")
