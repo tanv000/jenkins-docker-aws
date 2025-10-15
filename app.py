22729c0c877f080ea4ade590a9978584f9adc469
Settem Tanvi <144032915+tanv000@users.noreply.github.com>
2025-10-15 16:14:35 +0530

    trigger

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ def main():
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"Hello from the basic Python application!")
     print(f"The current UTC time is: {current_time}")
+    
 
 if __name__ == "__main__":
     main()
